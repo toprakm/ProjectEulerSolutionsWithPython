6d98266f0c639d59d001bd54695bc2b6eb786987
toprakm <40161009+toprakm@users.noreply.github.com>
2019-09-22 21:20:54 +0300

    problem0013
    solution

--- problem0013.py
+++ problem0013.py
@@ -1,6 +1,12 @@
 #   Work out the first ten digits of the sum of the following one-hundred 50-digit numbers.
 #   Numbers are in problem0013_nums.txt
 
+top=0
+add=0
 with open("problem0013_nums.txt") as file:
     for i in file:
-        print (i.strip())
+        add=int(i.strip())
+        top=top+add
+print(top)
+s=str(top)
+print("First 10 digits:",s[:10])
