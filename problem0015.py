01ec11ee357ff771346d8ccfe78c9f4959d6df78
toprakm <40161009+toprakm@users.noreply.github.com>
2019-10-14 11:16:11 +0300

    Create problem0015.py

---
+++ problem0015.py
@@ -0,0 +1,10 @@
+# 2^15 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
+#
+# What is the sum of the digits of the number 2^1000?
+
+a=2**1000
+stra=str(a)
+top=0
+for i in stra:
+    top=top+int(i)
+print(top)    
